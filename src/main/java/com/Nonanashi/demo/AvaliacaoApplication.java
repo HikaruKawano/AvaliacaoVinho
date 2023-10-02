6a3195068465ac59ad68276f3c74f3e49e8b4d46
Nonanashi <nonanashi@pop-os.localdomain>
2023-10-02 12:30:17 -0300

    Init

---
+++ src/main/java/com/Nonanashi/demo/AvaliacaoApplication.java
@@ -0,0 +1,13 @@
+package com.Nonanashi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliacaoApplication.class, args);
+	}
+
+}
